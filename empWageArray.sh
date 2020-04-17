4fe84ee2e8cc936037499cb4bf34bb64533cf9b0
meghamoree <meghamoree@gmail.com>
2020-04-17 09:29:41 +0530

    Add. Adding a program which computes the emp Wage Array empWageArray.sh to repository

---
+++ empWageArray.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+totalWorkHrs=0;
+totalWorkingDays=0;
+maxHrsInMonth=10;
+numberWorkingDays=20;
+
+function getEmpWage() {
+  local empHr=$1
+  echo $(($empHr*$empRatePerHr))
+}
+function getWorkHrs() {
+  local $empCheck=$1
+  case $empCheck in
+                    $isFullTime )
+                         empHrs=8
+                         ;;
+                    $isPartTime )
+                         empHrs=4
+                        ;;
+             *)
+                empHrs=0;
+               ;;
+        esac
+echo $empHrs
+}
+while [[ $totalWorkHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numberWorkingDays ]];
+do
+      ((totalworkingDays++))
+       empCheck=$(( RANDOM % 3));
+       empHrs="$( getWorkHrs $empCheck )"
+       totalWorkHrs=$(( $totalWorkHrs+$empCheck ));
+       dailyWage[$totalWorkingDays]="$( getEmpWage empHrs )"
+done
+      totalSalary=$(( $totalWorkHrs*$empHrs ));
+      echo ${dailyWage[@]}
+
+
+
+
+
+
+
+
+
+
