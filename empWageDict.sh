2fd79c6494353ee85046cc19125d5d86321e3f23
meghamoree <meghamoree@gmail.com>
2020-04-17 10:24:18 +0530

    https://github.com/meghamoree/Myprograms.git

---
+++ empWageDict.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -x
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+totalWorkHrs=0;
+totalWorkingDays=0;
+maxHrsInMonth=10;
+numberWorkingDays=20;
+
+declare -A dailyWage
+
+function getEmpWage() {
+     local empHr=$1
+     echo $(($empHr*$empRatePerHr))
+}
+
+function getWorkHrs() {
+  local $empCheck=$1
+  case $empCheck in
+                    $isFullTime )
+                         empHrs=8
+                         ;;
+                    $isPartTime )
+                         empHrs=4
+                        ;;
+             *)
+                empHrs=0;
+               ;;
+        esac
+echo $empHrs
+}
+while [[ $totalWorkHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numberWorkingDays ]];
+do
+      ((totalworkingDays++))
+       empCheck=$(( RANDOM % 3));
+       empHrs="$( getWorkHrs $empCheck )"
+       totalWorkHrs=$(( $totalWorkHrs+$empCheck ));
+       dailyWage[ "Day""$totalWorkingDays"]="$( getEmpWage $empHrs )"
+done
+      totalSalary=$(( $totalWorkHrs*$empRatePerHr ));
+      echo ${dailyWage[@]}
+      echo ${!dailyWage[@]}
+
+
+
