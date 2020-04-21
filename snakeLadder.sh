0fed8b06be73320b617f3427bbfb947637d52893
meghamoree <meghamoree@gmail.com>
2020-04-21 18:43:18 +0530

    Add.Adding a usecase 3 in snake and ladder program

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -2,11 +2,38 @@
 
 startPosition=0
 endPosition=100
-playerOnePosition=0
+
+function noPlay()
+{
+ 	playerOnePosition=$(($playerOnePosition+0))
+}
+
+function snake()
+{
+ 	playerOnePosition=$(($playerOnePosition-$diceOutput))
+}
+
+function ladder()
+{
+ 	playerOnePosition=$(($playerOnePosition+$diceOutput))
+}
 
 function play()
 {
-	diceOutput=$((RANDOM%6+1))
+	PlayerOnePosition=0
+        diceOutput=$((RANDOM%6+1))
+
+ if [[ $choice -eq 0 ]]
+                then
+                        noPlay
+                elif [[ $choice -eq 1 ]]
+                then
+                        snake
+                elif [[ $choice -eq 2 ]]
+                then
+                        ladder
+             fi
 echo $diceOutput
 }
 play
+
